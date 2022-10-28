341300bdb604550487c1b3edf1f5b4b58b21d2a7
dvyno <ced.david0704@gmail.com>
2022-10-27 19:24:03 -0700

    Move magic numbers

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,72 @@ import item.*;
 import environment.*;
 import entity.*;
 
+import constants.EntityValues;
+import constants.FilePaths;
 
+// TODO: Place varialbes for hardcoded values
 public class Main extends PApplet {
+    private static final String MAIN_CLASS = "Main";
+    private static final String FONT_FILE = "PressStart2P/PressStart2P.ttf";
+    private static final int FONT_SIZE = 48;
+
+    private static final int CHARACTER_COUNT = 8;
+    private static final String ELF_FILE = "elf";
+    private static final String KNIGHT_FILE = "knight";
+    private static final String LIZARD_FILE = "lizard";
+    private static final String WIZARD_FILE = "wizard";
+    private static final String MALE_EXTENSION = "_m";
+    private static final String FEMALE_EXTENSION = "_f";
+
+    private static final int FRAME_COUNT = EntityValues.DEFAULT_FRAME_COUNT;
+
+    private static final String MENU_FILE = "_menu_f";
+
+    private static final int WEAPON_COUNT = 8;
+    private static final String WEAPON_PATH = "assets/weapons/";
+    private static final String WEAPON_MENU_EXTENSION = "_menu" + FilePaths.PNG_EXTENSION;
+    private static final String AXE_FILE = "axe";
+    private static final String BATON_FILE = "baton_with_spikes";
+    private static final String GOLD_SWORD_FILE = "golden_sword";
+    private static final String KNIGHT_SWORD_FILE = "knight_sword";
+    private static final String GREEN_STAFF_FILE = "green_magic_staff";
+    private static final String RED_STAFF_FILE = "red_magic_staff";
+    private static final String KATANA_FILE = "katana";
+    private static final String SPEAR_FILE = "spear";
+
+    private static final String CSV_PATH = "src/resources/scores.csv";
+    private static final String CSV_DELIMITER = ",";
+    private static final String CSV_READ_ERROR = "Scores cannot be created.";
+    private static final String CSV_WRITE_ERROR = "Unable to save scores.";
+
+    private static final int START_STATE = 0;
+    private static final int SETTINGS_STATE = 1;
+    private static final int CHARACTER_SELECT_STATE = 2;
+    private static final int WEAPON_SELECT_STATE = 3;
+    private static final int PLAY_GAME_STATE = 4;
+    private static final int GAME_OVER_STATE = 5;
+    private static final int SCORE_BOARD_STATE = 6;
+    private static final int CREDITS_STATE = 7;
+
+    private static final char W_KEY = 'w';
+    private static final char A_KEY = 'a';
+    private static final char S_KEY = 's';
+    private static final char D_KEY = 'd';
+    private static final char ACTION_KEY = ' ';
+
+    private static final int MENU_OPTIONS_COUNT = 3;
+
+    private static final int MAX_NAME_LENGTH = 3;
+
+    private static final String TITLE = "Dungeons,\nDungeons and\nMore Dungeons";
+    private static final int TITLE_SIZE = 32;
+
+    private static final String START_OPTION = "Start";
+    private static final String SETTINGS_OPTION = "Settings";
+    private static final String CREDITS_OPTION = "Credits";
+    private static final int OPTION_SIZE = 24;
+
+
     // Number of Rows and Columns in Room
     private final int ROWS = 15;
     private final int COLS = 19;
@@ -58,7 +122,7 @@ public class Main extends PApplet {
     private ArrayList<Integer> scores;
 
     public static void main(String[] args) {
-        PApplet.main("Main");
+        PApplet.main(MAIN_CLASS);
     }
 
     public void settings() {
@@ -67,7 +131,7 @@ public class Main extends PApplet {
 
     public void setup() {
         // Create and Set Font
-        PressStart2P = createFont("PressStart2P/PressStart2P.ttf", 48);
+        PressStart2P = createFont(FONT_FILE, FONT_SIZE);
         textFont(PressStart2P);
 
         // Start Menu
@@ -76,23 +140,25 @@ public class Main extends PApplet {
         selectIndex = 0;
 
         // Character Selection Menu
-        characterFiles = new String[]{"elf", "elf", "knight", "knight", "lizard", "lizard", "wizard", "wizard"};
-        for (int i = 0; i < 8; i++) {
-            String gender;
-            if (i % 2 == 0) {
-                gender = "_m";
-            }
-            else {
-                gender = "_f";
-            }
-            String str = characterFiles[i];
-            characterFiles[i] = str + gender + "/" + str + gender;
+        characterFiles = new String[]{
+            ELF_FILE + MALE_EXTENSION, ELF_FILE + FEMALE_EXTENSION,
+            KNIGHT_FILE + MALE_EXTENSION, KNIGHT_FILE + FEMALE_EXTENSION,
+            LIZARD_FILE + MALE_EXTENSION, LIZARD_FILE + FEMALE_EXTENSION,
+            WIZARD_FILE + MALE_EXTENSION, WIZARD_FILE + FEMALE_EXTENSION
+        };
+
+        for (int i = 0; i < CHARACTER_COUNT; i++) {
+            String path = characterFiles[i];
+            characterFiles[i] = path + "/" + path;
         }
 
-        characterImages = new PImage[8][4];
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 4; j++) {
-                String path = "assets/" + characterFiles[i] + "_menu_f" + nf(j,1) + ".png";
+        // 8: number of characters
+        // 4: frames for each character
+        characterImages = new PImage[CHARACTER_COUNT][FRAME_COUNT];
+        for (int i = 0; i < CHARACTER_COUNT; i++) {
+            for (int j = 0; j < FRAME_COUNT; j++) {
+                String path = FilePaths.ASSETS_PATH + characterFiles[i] +
+                    MENU_FILE + nf(j,1) + FilePaths.PNG_EXTENSION;
                 characterImages[i][j] = loadImage(path);
             }
         }
@@ -101,11 +167,14 @@ public class Main extends PApplet {
         charSelectIndex = 0;
 
         // Weapon Selection Menu
-        weaponFiles = new String[]{"axe", "baton_with_spikes", "golden_sword", "knight_sword", "green_magic_staff", "red_magic_staff", "katana", "spear"};
+        weaponFiles = new String[]{
+            AXE_FILE, BATON_FILE, GOLD_SWORD_FILE, KNIGHT_SWORD_FILE,
+            GREEN_STAFF_FILE, RED_STAFF_FILE, KATANA_FILE, SPEAR_FILE
+        };
 
-        weaponImages = new PImage[8];
-        for (int i = 0; i < 8; i++) {
-            String path = "assets/weapons/" + weaponFiles[i] + "_menu.png";
+        weaponImages = new PImage[WEAPON_COUNT];
+        for (int i = 0; i < WEAPON_COUNT; i++) {
+            String path = WEAPON_PATH + weaponFiles[i] + WEAPON_MENU_EXTENSION;
             weaponImages[i] = loadImage(path);
         }
         weaponSelectIndex = 0;
@@ -116,7 +185,7 @@ public class Main extends PApplet {
         // Scoreboard
         names = new ArrayList<String>();
         scores = new ArrayList<Integer>();
-        scoreFile = "src/resources/scores.csv";
+        scoreFile = CSV_PATH;
         try {
             Scanner reader = new Scanner(new File(scoreFile));
             String line = "";
@@ -124,7 +193,7 @@ public class Main extends PApplet {
 
             while (reader.hasNext()) {
                 line = reader.nextLine();
-                values = line.split(",");
+                values = line.split(CSV_DELIMITER);
                 names.add(values[0]);
                 scores.add(Integer.valueOf(values[1]));
             }
@@ -132,79 +201,77 @@ public class Main extends PApplet {
             reader.close();
         }
         catch (Exception e) {
-            System.out.println("Scores cannot be created.");
+            System.out.println(CSV_READ_ERROR);
         }
     }
 
     public void draw() {
         switch (gameState) {
-            case 0:
+            case START_STATE:
                 startMenu();
                 break;
-            case 1:
+            case SETTINGS_STATE:
                 settingsMenu();
                 break;
-            case 2:
+            case CHARACTER_SELECT_STATE:
                 characterSelectionMenu();
                 break;
-            case 3:
+            case WEAPON_SELECT_STATE:
                 weaponSelectionMenu();
                 break;
-            case 4:
+            case PLAY_GAME_STATE:
                 playGame();
                 break;
-            case 5:
+            case GAME_OVER_STATE:
                 gameOver();
                 break;
-            case 6:
+            case SCORE_BOARD_STATE:
                 scoreBoard();
                 break;
-            case 7:
+            case CREDITS_STATE:
                 creditsMenu();
                 break;
         }
     }
 
     public void keyPressed() {
-        if (key == 'p') {
-            gameState = (gameState + 1) % 8;
-        }
+        
         switch (gameState) {
             // Start Menu
-            case 0:
-                if (key == 'w') {
-                    selectIndex = (selectIndex + 2) % 3;
+            case START_STATE:
+                if (key == W_KEY) {
+                    selectIndex = (selectIndex + MENU_OPTIONS_COUNT - 1) % MENU_OPTIONS_COUNT;
                 }
 
-                if (key == 's') {
-                    selectIndex = (selectIndex + 1) % 3;
+                if (key == S_KEY) {
+                    selectIndex = (selectIndex + 1) % MENU_OPTIONS_COUNT;
                 }
 
-                if (key == ' ') {
+                if (key == ACTION_KEY) {
                     if (selectIndex == 0) {
-                        gameState = 2;
+                        gameState = CHARACTER_SELECT_STATE;
                     }
                     else if (selectIndex == 1) {
-                        gameState = 1;
+                        gameState = SETTINGS_STATE;
                     }
                     else {
-                        gameState = 7;
+                        gameState = CREDITS_STATE;
                     }
                 }
                 break;
             // Settings Menu
-            case 1:
-                if (key == ' ') {
+            case SETTINGS_STATE:
+                if (key == ACTION_KEY) {
                     HitBox.toggle();
                 }
 
                 if (key == BACKSPACE) {
-                    gameState = 0;
+                    gameState = START_STATE;
                 }
                 break;
             // Character Select Menu
-            case 2:
-                if (key == 'w' || key == 's') {
+            case CHARACTER_SELECT_STATE:
+                if (key == W_KEY || key == S_KEY) {
                     if (selectIndex % 2 == 0) {
                         selectIndex++;
                     }
@@ -213,24 +280,24 @@ public class Main extends PApplet {
                     }
                     frameNum = 0;
                 }
-                else if (key == 'a') {
-                    selectIndex = (selectIndex + 6) % 8;
+                else if (key == A_KEY) {
+                    selectIndex = (selectIndex + 6) % CHARACTER_COUNT;
                     frameNum = 0;
                 }
-                else if (key == 'd') {
-                    selectIndex = (selectIndex + 2) % 8;
+                else if (key == D_KEY) {
+                    selectIndex = (selectIndex + 2) % CHARACTER_COUNT;
                     frameNum = 0;
                 }
 
-                if (key == ' ') {
-                    gameState = 3;
+                if (key == ACTION_KEY) {
+                    gameState = WEAPON_SELECT_STATE;
                     charSelectIndex = selectIndex;
                     selectIndex = 0;
                 }
                 break;
             // Weapon Select Menu
-            case 3:
-                if (key == 'w' || key == 's') {
+            case WEAPON_SELECT_STATE:
+                if (key == W_KEY || key == S_KEY) {
                     if (selectIndex % 2 == 0) {
                         selectIndex++;
                     }
@@ -239,40 +306,40 @@ public class Main extends PApplet {
                     }
                     frameNum = 0;
                 }
-                else if (key == 'a') {
-                    selectIndex = (selectIndex + 6) % 8;
+                else if (key == A_KEY) {
+                    selectIndex = (selectIndex + 6) % WEAPON_COUNT;
                     frameNum = 0;
                 }
-                else if (key == 'd') {
-                    selectIndex = (selectIndex + 2) % 8;
+                else if (key == D_KEY) {
+                    selectIndex = (selectIndex + 2) % WEAPON_COUNT;
                     frameNum = 0;
                 }
 
-                if (key == ' ') {
-                    gameState = 4;
+                if (key == ACTION_KEY) {
+                    gameState = PLAY_GAME_STATE;
                     weaponSelectIndex = selectIndex;
                     selectIndex = 0;
                     createGame();
                 }
                 break;
             // Game
-            case 4:
+            case PLAY_GAME_STATE:
                 if (!player.isHit()) {
-                    if (key == 'w' && !player.isMovingDown()) {
+                    if (key == W_KEY && !player.isMovingDown()) {
                         player.setMoveUp(true);
-                        player.setRecentKey('w');
+                        player.setRecentKey(W_KEY);
                     }
-                    if (key == 'a' && !player.isMovingRight()) {
+                    if (key == A_KEY && !player.isMovingRight()) {
                         player.setMoveLeft(true);
-                        player.setRecentKey('a');
+                        player.setRecentKey(A_KEY);
                     }
-                    if (key == 's' && !player.isMovingUp()) {
+                    if (key == S_KEY && !player.isMovingUp()) {
                         player.setMoveDown(true);
-                        player.setRecentKey('s');
+                        player.setRecentKey(S_KEY);
                     }
-                    if (key == 'd' && !player.isMovingLeft()) {
+                    if (key == D_KEY && !player.isMovingLeft()) {
                         player.setMoveRight(true);
-                        player.setRecentKey('d');
+                        player.setRecentKey(D_KEY);
                     }
         
                     if (key == ' ') {
@@ -281,9 +348,10 @@ public class Main extends PApplet {
                 }
                 break;
             // Game Over
-            case 5:
-                if (key == ' ') {
+            case GAME_OVER_STATE:
+                if (key == ACTION_KEY) {
                     int i = scores.size() - 1;
+                    // linear search scores
                     while (i >= 0 && (score > scores.get(i))) {
                         i--;
                     }
@@ -291,25 +359,25 @@ public class Main extends PApplet {
                     scores.add(i + 1, score);
 
                     saveScores();
-                    gameState = 6;
+                    gameState = SCORE_BOARD_STATE;
                 }
                 else if (key != CODED && name.length() > 0 && key == BACKSPACE) {
                     name = name.substring(0, name.length() - 1);
                 }
-                else if (key != CODED && name.length() < 3 && key != BACKSPACE) {
+                else if (key != CODED && name.length() < MAX_NAME_LENGTH && key != BACKSPACE) {
                     name = (name + key).toUpperCase();
                 }
                 break;
             // Score Board
-            case 6:
-                if (key == ' ') {
-                    gameState = 0;
+            case SCORE_BOARD_STATE:
+                if (key == ACTION_KEY) {
+                    gameState = START_STATE;
                 }
                 break;
             // Credits Menu
-            case 7:
-                if (key == ' ') {
-                    gameState = 0;
+            case CREDITS_STATE:
+                if (key == ACTION_KEY) {
+                    gameState = START_STATE;
                 }
                 break;
         }
@@ -317,18 +385,18 @@ public class Main extends PApplet {
 
     public void keyReleased() {
         switch (gameState) {
-            case 4:
+            case PLAY_GAME_STATE:
                 if (!player.isHit()) {
-                    if (key == 'w' && !player.isMovingDown()) {
+                    if (key == W_KEY && !player.isMovingDown()) {
                         player.setMoveUp(false);
                     }
-                    if (key == 'a' && !player.isMovingRight()) {
+                    if (key == A_KEY && !player.isMovingRight()) {
                         player.setMoveLeft(false);
                     }
-                    if (key == 's' && !player.isMovingUp()) {
+                    if (key == S_KEY && !player.isMovingUp()) {
                         player.setMoveDown(false);
                     }
-                    if (key == 'd' && !player.isMovingLeft()) {
+                    if (key == D_KEY && !player.isMovingLeft()) {
                         player.setMoveRight(false);
                     }
 
@@ -347,14 +415,15 @@ public class Main extends PApplet {
         try {
             PrintWriter writer = new PrintWriter(new File(scoreFile));
             String line = "";
-            for (int i = 0; i < scores.size(); i++) {
-                line = names.get(i) + "," + scores.get(i);
+            int length = scores.size();
+            for (int i = 0; i < length; i++) {
+                line = names.get(i) + CSV_DELIMITER + scores.get(i);
                 writer.println(line);
             }
             writer.close();
         }
         catch (Exception e) {
-            System.out.println("Unable to save scores.");
+            System.out.println(CSV_WRITE_ERROR);
         }
     }
 
@@ -473,18 +542,19 @@ public class Main extends PApplet {
         fill(0xFFFFFFFF);
     }
 
+    // TODO: Place magic numbers in variables
     // State 0
     private void startMenu() {
         menuBackground();
         textAlign(CENTER, CENTER);
-        textSize(32);
-        text("Dungeons,\nDungeons and\nMore Dungeons", width / 2, height / 6 + 24);
+        textSize(TITLE_SIZE);
+        text(TITLE, width / 2, height / 6 + 24);
 
         // Menu options
-        textSize(24);
-        optionTexts[0] = "Start";
-        optionTexts[1] = "Settings";
-        optionTexts[2] = "Credits";
+        textSize(OPTION_SIZE);
+        optionTexts[0] = START_OPTION;
+        optionTexts[1] = SETTINGS_OPTION;
+        optionTexts[2] = CREDITS_OPTION;
 
         optionColors[0] = 0xFFFFFFFF;
         optionColors[1] = 0xFFFFFFFF;
